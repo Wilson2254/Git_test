88ec28d832d66df60f1b074e1d9e56abe03ad5e8
malistrix <evdokimova_v_a@mail.ru>
2019-04-22 00:14:09 +0300

    red

--- Git_test/Form1.Designer.cs
+++ Git_test/Form1.Designer.cs
@@ -34,12 +34,14 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.Brown;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
